1208b56dbff02e87f0792bda37680b6e5fefe0f8
GuyWhoCode <34526187+GuyWhoCode@users.noreply.github.com>
2026-03-08 16:12:38 -0700

    feat: sample data from JSON file

--- app/add/ItemsConfirmationTable.tsx
+++ app/add/ItemsConfirmationTable.tsx
@@ -6,7 +6,8 @@ import { useItems } from "./ItemsContext";
 
 function ItemsConfirmationTable() {
     const { items, deleteItems } = useItems();
-    
+    console.log("Current items in context:", items);
+
     return (
         <div>
             <DataTable columns={columns} data={items} onDeleteRows={deleteItems} />
--- app/add/ItemsContext.tsx
+++ app/add/ItemsContext.tsx
@@ -1,7 +1,16 @@
 "use client";
-
-import { createContext, Dispatch, ReactNode, SetStateAction, useContext, useState } from "react";
+import {
+    createContext,
+    Dispatch,
+    ReactNode,
+    SetStateAction,
+    useContext,
+    useState,
+} from "react";
 import { ItemEntry } from "@/types";
+import sampleData from "../../deliverables/sampleData.json";
+
+const isSampleDataSet = process.env.NEXT_PUBLIC_SAMPLE_DATA_SET === "true";
 
 type ItemsContextType = {
     items: ItemEntry[];
@@ -13,7 +22,9 @@ type ItemsContextType = {
 const ItemsContext = createContext<ItemsContextType | null>(null);
 
 export function ItemsProvider({ children }: { children: ReactNode }) {
-    const [items, setItems] = useState<ItemEntry[]>([]);
+    const [items, setItems] = useState<ItemEntry[]>(
+        isSampleDataSet ? sampleData : [],
+    );
 
     const addItem = (item: ItemEntry) => {
         setItems((prev) => [...prev, item]);
